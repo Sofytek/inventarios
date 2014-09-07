2e910d903edbf32a28a68ff3ba8e29c7321677de
karenv0207 <karenv2007@hotmail.com>
2014-09-07 11:49:41 -0500

    Línea
    Archivos para la creación de líneas

---
+++ Logica/Model/Linea.php
@@ -0,0 +1,62 @@
+<?php
+require_once ('../bdcontrol/IDataAccess.php');
+
+class Linea implements IDataAccess{
+
+	private $idLinea;
+	private $nombre;
+	private $activo;
+
+	public function __construct($arrlista = null){
+		if($arrlista != null)
+		{
+			$this->setData($arrlista);
+		}
+	}
+	
+	//Setters and Getters
+	Public function setIdLinea($idLinea){
+		$this->idLinea = $idLinea;
+	}
+
+	public function getIdLinea(){
+		return $idLinea; 
+	}
+
+	public function setNombre($nombre){
+		$this->nombre = $nombre;
+	}
+
+	function &getNombre(){
+		return $nombre;
+	}
+
+	public function setActivo($activo){
+		$this->activo = $activo;
+	}
+
+	public function getActivo(){
+		return $activo;
+	}
+
+	//implementacion de los metodos de IDataAcces
+	public function getTitle(){
+		return "Linea";
+	}
+
+	public function getData()
+	{
+		$columName = array("idlinea", "nombre", "activo");
+		$values = array($this->idLinea, $this->nombre, $this->activo);
+    	return array($columName, $values);
+	}
+
+	public function setData($arrayData)
+	{
+		$this->idEmpresa = $arrayData['idempresa'];
+		$this->nombre = $arrayData['nombre'];
+		$this->activo = $arrayData['activo'];
+	}
+}
+
+?>
